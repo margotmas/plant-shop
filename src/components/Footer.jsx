04247dc009090e2cb374901fccff8bc0b6eec30c
mathi <sinoir.mathieu@gmail.com>
2023-12-18 22:14:59 +0100

    ajout css + margin + borderRadius

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer({ infos }) {
   return (
     <>
 
-      <section className="BackgroundColorFooter">
-        <ul className="UlFooter">
-          <li className="LiFooter">{infos.FirstName}</li>
-        </ul>
-      </section>
+
+      <ul className="UlFooter">
+        <li className="LiFooter">{infos.FirstName}</li>
+      </ul>
+
 
 
     </>
