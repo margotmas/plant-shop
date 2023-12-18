590d1f7d4f1b6f40a824bff469bcb1c3536c54d4
mathi <sinoir.mathieu@gmail.com>
2023-12-18 20:29:03 +0100

    ajout du footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@
 // import Header from ".components/Header"
 import Cards from './components/Cards'
 import plantsToSell from './data/plantsToSell'
+import Footer from './components/Footer';
 // import style from './styles/App.module.css'
+
+
+const NameOfWorkers = [
+  {
+    FirstName: "Alexia",
+
+  },
+  {
+    FirstName: 'Margot',
+
+  },
+
+  {
+    FirstName: 'Mathieu',
+
+  },
+]
 
 
 function App() {
@@ -16,8 +34,14 @@ function App() {
           image={plant.img}
         />
       ))}
+      <h2>développé par</h2>
+      {NameOfWorkers.map((NameOfWorker, index) => (
+        <Footer key={index} infos={NameOfWorker} />
+      ))}
+
+
     </>
   );
 }
-console.log(plantsToSell)
+
 export default App
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
-function Footer() {
 
-    return (
-      <>
-  
-      </>
-    )
-  }
-  
-  export default Footer
+function Footer({ infos }) {
+
+  return (
+    <>
+
+      <div>
+        <ul>
+          <li>{infos.FirstName}</li>
+        </ul>
+      </div>
+
+
+    </>
+  )
+}
+
+export default Footer
